7ec40eac960118a59dda3f15d914bc30a24c2f6e
ShubhamSChede <shubhamchede1602@gmail.com>
2025-02-05 00:20:21 +0530

    routing issue 3

--- src/app/admin/control/page.js
+++ src/app/admin/control/page.js
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect } from 'react';
 import { Trash2, Edit, Save, X } from 'lucide-react';
 import { Alert, AlertDescription } from '@/components/ui/alert';
--- src/app/admin/dashboard/page.js
+++ src/app/admin/dashboard/page.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import DashboardClient from './../../../components/Dashboard';
 
 export default function ControlPage() {
